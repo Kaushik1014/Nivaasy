baf487d24f1bfa0d2d995c088c202f5fa07d338a
Kaushik Gautam <gautamkaushik1014@gmail.com>
2026-03-10 18:50:33 +0530

    Add async wrapper and centralized error handling
    Add utils/wrapAsync and utils/ExpressError and update routes to use wrapAsync for automatic async error propagation. Validate req.body.listing in create/update routes and throw ExpressError on bad input. Add 404 handler and a centralized error handler in app.js to send proper status and messages. Also remove the numeric-only constraint from the price input in views/listings/new.ejs.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 main()
 .then(()=>{console.log("connection successful")})
@@ -37,39 +39,50 @@ app.get("/listings/new" , (req, res)=>{
 });
 
 //show route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res, next) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
-});
+}));
 
 //create route
-app.post("/listings", async(req, res)=>{
+app.post("/listings", wrapAsync(async(req, res, next)=>{
+    if(!req.body.listing) throw new ExpressError("Send valid data for listing", 400);
     const newListing = new Listing(req.body.listing); 
     await newListing.save();
     res.redirect("/listings");
-});
+})) ;
 
 //Edit route
-app.get("/listings/:id/edit",async(req, res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req, res, next)=>{
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-})
+}))
 
 //update route
-app.put("/listings/:id" , async(req, res)=>{
+app.put("/listings/:id" , wrapAsync(async(req, res, next)=>{
+    if (!req.body.listing) throw new ExpressError("Send valid data for listing", 400);
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id ,{...req.body.listing});
     res.redirect(`/listings/${ id }`);
-});
+}));
 
 //delete route
-app.delete("/listings/:id", async(req,res)=>{
+app.delete("/listings/:id", wrapAsync(async(req,res, next)=>{
     let { id } = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-}); 
+})); 
+
+app.use((req, res, next)=>{
+  next(new ExpressError("Page Not Found!", 404));
+});
+
+app.use((err, req, res, next)=>{
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).send(message);
+});
 
 app.listen(8080, ()=>{
     console.log("server is listening on port 8080");
