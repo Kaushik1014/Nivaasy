c68f890f53fd1a310fa940f210e27f13cadfa2d0
Kaushik Gautam <gautamkaushik1014@gmail.com>
2026-03-12 22:13:58 +0530

    Add reviews support: schema, route, form
    Integrate reviews into listings: add a reviews array (ObjectId ref to Review) to the Listing schema, require the Review model in app.js, and add a POST /listings/:id/reviews route that creates a Review, pushes it onto the listing.reviews array, and saves both. Also update the listing show view to display the listing title and include a review submission form (rating and comment) that posts to the new route.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 main()
 .then(()=>{console.log("connection successful")})
@@ -83,6 +84,17 @@ app.delete("/listings/:id", wrapAsync(async(req,res, next)=>{
     let deleteListing = await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
 })); 
+
+//review POST route
+app.post("/listings/:id/reviews", async (req, res, next)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
+});
+
 
 app.use((req, res, next)=>{
   next(new ExpressError("Page Not Found!", 404));
